283d738546b883584b1cd85287a089d5624ce301
Thomas William Komp <twkomp@gmail.com>
2018-06-14 13:34:36 -0400

    Second Commit

--- app/src/main/java/com/example/thomaskomp/uielementsexamples/MainActivity.java
+++ app/src/main/java/com/example/thomaskomp/uielementsexamples/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.thomaskomp.uielementsexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
+    private EditText editTEXT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
